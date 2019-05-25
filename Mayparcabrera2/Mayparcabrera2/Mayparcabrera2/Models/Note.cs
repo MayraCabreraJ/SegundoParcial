e222b238c39f7b88d7f1ee68f00ac84d9f728597
Mayra Veronica Cabrera Jaldin <mayricienta1@hotmail.com>
2019-05-25 11:36:00 -0400

    commit principio y fin

---
+++ Mayparcabrera2/Mayparcabrera2/Mayparcabrera2/Models/Note.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mayparcabrera2.Models;
+
+
+namespace Mayparcabrera2.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+
+    class Note
+    {
+        [Key]
+        public int NotesId { get; set; }
+        public string Contents { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public Note()
+        {
+            this.CreatedDate = DateTime.Now;
+            this.ModifiedDate = DateTime.Now;
+        }
+    }
+}
---
+++ Mayparcabrera2/Mayparcabrera2/Mayparcabrera2/ViewModels/ApiViewModel.cs
@@ -0,0 +1,81 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using App.Services;
+using Mayparcabrera2.Models;
+using Mayparcabrera2.ViewModels;
+using App.ViewModels;
+
+namespace Mayraparcabrera2.ViewModels
+{
+   
+
+    public class ApiViewModel :BaseViewModel
+    {
+        #region Attributes
+        string content;
+       
+       
+        ApiServices apiService;
+        #endregion
+
+        #region Properties
+        public string Note
+        {
+            get
+            {
+                return this.content;
+            }
+            set
+            {
+                SetValue(ref this.content, value);
+
+            }
+        }
+        
+        
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(cmdLogin);
+            }
+        }
+
+        private async void cmdLogin()
+        {
+            if (String.IsNullOrEmpty(content))
+            {
+                await App.Current.MainPage.DisplayAlert(
+                                "Ingrese su Nota",
+                                "Accept");
+                return;
+            }
+           
+
+          
+            TokenResponse token = await this.apiService.
+                   "https://notesplc.azurewebsites.net",
+                    this.Note);
+
+
+
+
+            MainViewModel mainViewModel = MainViewModel.GetInstance();
+            mainViewModel.Token = token.AccessToken;
+            mainViewModel.TokenType = token.TokenType;
+
+     
+        }
+        #endregion
+
+        public ApiViewModel()
+        {
+
+        }
+    }
+}
